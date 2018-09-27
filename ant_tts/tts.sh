5fe3785586dcf8438f6bee88560b7334642fa76c
Hello World <hello@world>
2018-09-27 20:27:57 +1000

    modifying tts.sh

--- ant_tts/tts.sh
+++ ant_tts/tts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CARD=$(aplay -l | grep "card [0-9]: Pro \[SB X-Fi Surround 5.1 Pro\], device [0-9]: USB Audio \[USB Audio\]$" | cut -c 6)
 SPEAK="'$*'"
-espeak -s 150 -v en+f4 "$SPEAK" --stdout | aplay -c 1 -t wav -r 22050 --device=plughw:$CARD
+espeak -s 125 -v en+f4 "$SPEAK" --stdout | aplay -c 1 -t wav -r 22050 --device=plughw:$CARD
